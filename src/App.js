e16aa46c12d3e1789714ddd3a8f2717fee50a02c
Stefan Bjelogrlic <stefanbjelogrlic1999@gmail.com>
2023-01-23 08:51:36 +0100

    dodavanje linka

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Teams from './pages/Teams';
 function App() {
   const[teams,setTeams]= useState([]);
   const fetchTeams = async ()=>{
-    const response = await fetch('');
+    const response = await fetch('https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League');
     const data  = await response.json();
     setTeams(data.teams);
     console.log(data.teams);
